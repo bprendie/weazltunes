c7b70174138a19714713227e85853ca2c22a9cc2
Bob Prendergast <bobprendie@gmail.com>
2026-05-16 21:44:23 -0400

    Split TUI playback lifecycle

--- internal/tui/actions.go
+++ internal/tui/actions.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/bprendie/weazltunes/internal/audio"
 	"github.com/bprendie/weazltunes/internal/directory"
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
@@ -24,62 +23,6 @@ func (m *Model) showMyStations() {
 	m.setStations(myStations(m.cfg))
 	m.status = "my stations"
 	m.err = ""
-}
-
-func (m *Model) play(st directory.Station) {
-	if err := m.player.Play(st.URL); err != nil {
-		m.err = err.Error()
-		return
-	}
-	m.playing = &st
-	m.paused = false
-	m.startMeter(st.URL)
-	m.status = "playing " + st.Name
-	m.err = ""
-}
-
-func (m *Model) togglePause() {
-	paused, err := m.player.TogglePause()
-	if err != nil {
-		m.err = err.Error()
-		return
-	}
-	m.paused = paused
-	if paused {
-		m.stopMeter()
-		m.status = "paused"
-		return
-	}
-	if m.playing != nil {
-		m.startMeter(m.playing.URL)
-		m.status = "playing " + m.playing.Name
-	}
-}
-
-func (m *Model) stop() {
-	m.player.Stop()
-	m.stopMeter()
-	m.playing = nil
-	m.paused = false
-	m.status = "stopped"
-}
-
-func (m *Model) startMeter(url string) {
-	m.stopMeter()
-	meter, err := audio.StartMeter(url)
-	if err != nil {
-		m.energy = audio.Sample{}
-		return
-	}
-	m.meter = meter
-}
-
-func (m *Model) stopMeter() {
-	if m.meter != nil {
-		m.meter.Stop()
-	}
-	m.meter = nil
-	m.energy = audio.Sample{}
 }
 
 func (m *Model) setStations(stations []directory.Station) {
